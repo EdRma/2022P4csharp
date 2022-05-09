930ce060e06c7b2678a7316703a6931c6a5de8a5
EdRma <34298@ma-web.nl>
2022-05-09 14:48:08 +0200

    solution

---
+++ 'Csharp2Opdrachten/'Csharp2Opdrachten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Csharp2Opdrachten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
