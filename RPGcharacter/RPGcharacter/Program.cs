e9e533af7f3dc25bef4673d1b248d04dca2cd7bb
EdRma <34298@ma-web.nl>
2022-06-17 13:21:17 +0200

    opdrachten

---
+++ RPGcharacter/RPGcharacter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RPGcharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rpgcharacter hero = new Rpgcharacter(999, 9999, "cloudstrife");
+            Console.WriteLine(hero.name);
+            Console.WriteLine(hero.str);
+            Console.WriteLine(hero.hp);
+            Console.WriteLine();
+        }
+        internal class Rpgcharacter
+        {
+            internal readonly string name;
+            internal int str;
+            internal int hp;
+            internal Rpgcharacter(int hp, int str, string name)
+            {
+                this.hp = hp;
+                this.str = str;
+                this.name = "cloudstrife";
+
+            }
+        }
+    }
+}
---
+++ enumopdracht/enumopdracht/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace enumopdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        internal enum colors
+        {
+            Red,
+            Green,
+            Blue,
+            Yellow
+        }
+    }
+}
+
+
